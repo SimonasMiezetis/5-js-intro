56c6eb66b35cf80219d95097f15722e50ad77e72
SimonasMiezetis <76005049+SimonasMiezetis@users.noreply.github.com>
2021-01-05 17:16:37 +0200

    JS basics

---
+++ js/kintamieji.js
@@ -0,0 +1,49 @@
+/*
+Kintamuju iniciavimo budai:
+- var (senas, nenaudoti)
+- let (jei del kazkokios priezasties nenaudojam "const", tada galim naudoti si "let")
+- const (prioritetinis pasirinkimas visais atvejais)
+Kintamuju saugomos reiksmes tipai:
+- skaicius (number)
+- tekstas (string)
+- logine reiksme (boolean)
+- sarasas (array [] )
+- objektas (object {})
+- funkcija (function () {} )
+*/
+
+const gimimoMetai = 1992;
+console.log(gimimoMetai);
+const gimimoMenesis = 05;
+console.log(gimimoMenesis);
+const gimimoDiena = 23;
+console.log(gimimoDiena);
+
+console.log('---------------------');
+
+const valstybe = 'Lietuva';
+console.log(valstybe);
+const miestas = 'Klaipeda';
+console.log(miestas);
+const gatve = 'Taikos Pr.';
+console.log(gatve);
+
+console.log('---------------------');
+
+const petriukoPazymiai = [10,5,7,9,6];
+console.log(petriukoPazymiai);
+const jonukoPazymiai = [9,9,7,8,6];
+console.log(jonukoPazymiai);
+const dariukoPazymiai = [10,10,10,10,2];
+console.log(dariukoPazymiai);
+
+console.log('---------------------');
+
+const petriukoTvarkarastis = ['Lietuviu k.','Anglu k.','Matematika','Matematika','Chemija'];
+console.log(petriukoTvarkarastis);
+const jonukoTvarkarastis = ['Anglu k.','Anglu k.','Chemija','Chemija','Fizika'];
+console.log(jonukoTvarkarastis);
+const dariukoTvarkarastis = ['Chemija','Fizika','Metematika','Matematika','Anglu k.'];
+console.log(dariukoTvarkarastis);
+
+console.log('---------------------');
---
+++ js/veiksmai.js
@@ -0,0 +1,84 @@
+const sum = gimimoMetai + gimimoMenesis + gimimoDiena;
+console.log(sum);
+
+console.log('---------------------');
+
+const space = ', ';
+const adresas = valstybe + space + miestas + space + gatve;
+console.log(adresas);
+
+console.log('---------------------');
+
+let petriukoIndex = 0;
+let petriukoSuma = 0;
+
+petriukoSuma += petriukoPazymiai[petriukoIndex++];
+petriukoSuma -= petriukoPazymiai[petriukoIndex++];
+petriukoSuma += petriukoPazymiai[petriukoIndex++];
+petriukoSuma -= petriukoPazymiai[petriukoIndex++];
+petriukoSuma += petriukoPazymiai[petriukoIndex++];
+console.log(petriukoSuma);
+
+console.log('---------------------');
+
+let jonukoIndex = 0;
+let jonukoSuma = 0;
+
+jonukoSuma += jonukoPazymiai[jonukoIndex++];
+jonukoSuma -= jonukoPazymiai[jonukoIndex++];
+jonukoSuma += jonukoPazymiai[jonukoIndex++];
+jonukoSuma -= jonukoPazymiai[jonukoIndex++];
+jonukoSuma += jonukoPazymiai[jonukoIndex++];
+console.log(jonukoSuma);
+
+console.log('---------------------');
+
+let dariukoIndex = 0;
+let dariukoSuma = 0;
+
+dariukoSuma += dariukoPazymiai[dariukoIndex++];
+dariukoSuma -= dariukoPazymiai[dariukoIndex++];
+dariukoSuma += dariukoPazymiai[dariukoIndex++];
+dariukoSuma -= dariukoPazymiai[dariukoIndex++];
+dariukoSuma += dariukoPazymiai[dariukoIndex++];
+console.log(dariukoSuma);
+
+console.log('---------------------');
+
+let petriukoPamokuSarasas = "";
+let index1 = 4;
+
+petriukoPamokuSarasas += petriukoTvarkarastis[index1--] + space;
+petriukoPamokuSarasas += petriukoTvarkarastis[index1--] + space;
+petriukoPamokuSarasas += petriukoTvarkarastis[index1--] + space;
+petriukoPamokuSarasas += petriukoTvarkarastis[index1--] + space;
+petriukoPamokuSarasas += petriukoTvarkarastis[index1--] + space;
+console.log(petriukoPamokuSarasas);
+
+console.log('---------------------');
+
+let jonukoPamokuSarasas = "";
+let index2 = 4;
+
+jonukoPamokuSarasas += jonukoTvarkarastis[index2--] + space;
+jonukoPamokuSarasas += jonukoTvarkarastis[index2--] + space;
+jonukoPamokuSarasas += jonukoTvarkarastis[index2--] + space;
+jonukoPamokuSarasas += jonukoTvarkarastis[index2--] + space;
+jonukoPamokuSarasas += jonukoTvarkarastis[index2--] + space;
+console.log(jonukoPamokuSarasas);
+
+console.log('---------------------');
+
+let dariukoPamokuSarasas = "";
+let index3 = 4;
+
+dariukoPamokuSarasas += dariukoTvarkarastis[index3--] + space;
+dariukoPamokuSarasas += dariukoTvarkarastis[index3--] + space;
+dariukoPamokuSarasas += dariukoTvarkarastis[index3--] + space;
+dariukoPamokuSarasas += dariukoTvarkarastis[index3--] + space;
+dariukoPamokuSarasas += dariukoTvarkarastis[index3--] + space;
+console.log(dariukoPamokuSarasas);
+
+console.log('---------------------');
+
+
